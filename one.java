bff7a7bb57fd3ec365d3ddd8fb47ff1157bb045c
PranayBhatnagar <bhatnagarpranay@outlook.com>
2024-07-17 17:41:45 +0530

    sum of all term sof an array

---
+++ one.java
@@ -0,0 +1,10 @@
+public class one{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        int sum = 0;
+        for(int i=0;i<5;i++){
+            sum += arr[i];
+        }
+        System.out.println("Sum of values are: " + sum);
+    }
+}
