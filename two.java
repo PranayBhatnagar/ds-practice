ed0f83b586d731f6fca806f3f2fa906636c3eb78
PranayBhatnagar <bhatnagarpranay@outlook.com>
2024-07-17 17:44:58 +0530

    largest in an array

--- two.java
+++ two.java
@@ -1,6 +1,14 @@
 public class two {
     public static void main(String[] args) {
-        
+        int[] arr = {1,2,3,4,5};
+        int temp = arr[0];
+        int largest = 0;
+        for(int i=0;i<5;i++){
+            if(arr[i]>temp){
+                largest = arr[i];
+            }
+        }
+        System.out.println("Largest number is: " + largest);
     }
     
 }
